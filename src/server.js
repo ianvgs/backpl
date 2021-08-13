7c9ae7b2712a7f66d28ef73fc345ebb607833e03
Ian Guedes <82589606+ianvgs@users.noreply.github.com>
2021-08-12 23:03:08 -0300

    sync

--- src/server.js
+++ src/server.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 3333;
 
 try {  
     //DB.SEQUELIZE.ACAO  
+
     db.sequelize.authenticate().then(()=>{
+        db.sequelize.sync()
         const app = express();
         app.listen(port, ()=>{ console.log(`voê está acessando a porta ${port}`)})
     })     
